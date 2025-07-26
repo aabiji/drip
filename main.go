9c03ad37024f1cb2ad5354bb8aacd3c466394152
Abigail Adegbiji <adegbijiabigail@gmail.com>
2025-07-26 11:01:59 -0600

    basic error tray

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"image"
 	"image/color"
@@ -23,7 +24,7 @@ import (
 const ( // pages
 	HOME_PAGE = iota
 	SETTINGS_PAGE
-	PROGRSSS_PAGE
+	PROGRESS_PAGE
 )
 
 const ( // icons
@@ -44,8 +45,9 @@ const ( // buttons
 )
 
 type FileEntry struct {
-	name    string
-	clicked widget.Clickable
+	name     string
+	clicked  widget.Clickable
+	progress float32
 }
 
 type UI struct {
@@ -54,6 +56,9 @@ type UI struct {
 
 	filesList *widget.List
 	files     []FileEntry
+
+	errorClicks []widget.Clickable
+	errors      []error
 
 	trustOption  widget.Bool
 	notifyOption widget.Bool
@@ -64,19 +69,6 @@ type UI struct {
 
 	downloadPath string
 	currentPage  int
-}
-
-func (ui *UI) switchPages(gtx C) {
-	if ui.buttons[PAGE_SWITCHER].Clicked(gtx) {
-		if ui.currentPage == HOME_PAGE {
-			ui.currentPage = SETTINGS_PAGE
-		} else if ui.currentPage == SETTINGS_PAGE {
-			ui.currentPage = HOME_PAGE
-		} else if ui.currentPage == PROGRSSS_PAGE {
-			ui.currentPage = HOME_PAGE
-			// TODO: cancel sending...
-		}
-	}
 }
 
 type C = layout.Context
@@ -117,7 +109,7 @@ func initUI() *UI {
 		peersList:    &widget.List{List: layout.List{Axis: layout.Vertical}},
 		filesList:    &widget.List{List: layout.List{Axis: layout.Vertical}},
 		downloadPath: "~/Downloads",
-		currentPage:  HOME_PAGE,
+		currentPage:  PROGRESS_PAGE,
 	}
 
 	iconData := [][]byte{
@@ -131,16 +123,40 @@ func initUI() *UI {
 		ui.icons = append(ui.icons, icon)
 	}
 
+	ui.addError(errors.New("error #1"))
+	ui.addError(errors.New("error #2"))
+
 	for i := 0; i < BTNS_END-BTNS_START; i++ {
 		ui.buttons = append(ui.buttons, widget.Clickable{})
 	}
 
 	for i := 0; i < 5; i++ {
 		ui.peerChecks = append(ui.peerChecks, widget.Bool{})
-		ui.files = append(ui.files, FileEntry{name: fmt.Sprintf("File %d", i)})
+		ui.files = append(ui.files, FileEntry{
+			name:     fmt.Sprintf("File %d", i),
+			progress: -1.0,
+		})
 	}
 
 	return ui
+}
+
+func (ui *UI) switchPages(gtx C) {
+	if ui.buttons[PAGE_SWITCHER].Clicked(gtx) {
+		if ui.currentPage == HOME_PAGE {
+			ui.currentPage = SETTINGS_PAGE
+		} else if ui.currentPage == SETTINGS_PAGE {
+			ui.currentPage = HOME_PAGE
+		} else if ui.currentPage == PROGRESS_PAGE {
+			ui.currentPage = HOME_PAGE
+			// TODO: cancel sending...
+		}
+	}
+}
+
+func (ui *UI) addError(err error) {
+	ui.errors = append(ui.errors, err)
+	ui.errorClicks = append(ui.errorClicks, widget.Clickable{})
 }
 
 func loadFont(path string) ([]font.FontFace, error) {
@@ -171,29 +187,9 @@ func drawIcon(data []byte, size int, lightMode bool) func(C) D {
 	}
 }
 
-func drawFileEntry(gtx C, theme T, file *FileEntry) D {
-	return layout.Flex{
-		Alignment: layout.Start,
-		Axis:      layout.Horizontal,
-		Spacing:   layout.SpaceBetween,
-	}.Layout(gtx,
-		layout.Rigid(func(gtx C) D {
-			return material.Label(theme, 16, file.name).Layout(gtx)
-		}),
-
-		layout.Rigid(func(gtx C) D {
-			remove, err := widget.NewIcon(icons.NavigationClose)
-			if err != nil {
-				panic(err)
-			}
-			return material.IconButton(theme, &file.clicked, remove, "Remove file").Layout(gtx)
-		}),
-	)
-}
-
 /*
 TODO:
-header, progressbar, list, div, files selection, notification
+files selection, notification
 */
 
 func drawFrame(ui *UI, gtx C, theme T) {
@@ -232,13 +228,90 @@ func drawFrame(ui *UI, gtx C, theme T) {
 
 					if ui.currentPage == HOME_PAGE {
 						return drawHomePage(gtx, theme, ui)
+					} else if ui.currentPage == PROGRESS_PAGE {
+						return drawProgressPage(gtx, theme, ui)
 					} else {
 						return drawSettingsPage(gtx, theme, ui)
 					}
 				})
 			})
 		}),
+
+		// error tray
+		layout.Stacked(func(gtx C) D {
+			return layout.Center.Layout(gtx, func(gtx C) D {
+				width := gtx.Constraints.Max.X * 30 / 100
+				height := gtx.Constraints.Max.Y * 30 / 100
+
+				return layout.Inset{}.Layout(gtx, func(gtx C) D {
+					gtx.Constraints = layout.Exact(image.Pt(width, height))
+					return drawErrorTray(gtx, theme, ui)
+				})
+			})
+		}),
 	)
+}
+
+func drawErrorTray(gtx C, theme T, ui *UI) D {
+	widgets := []layout.FlexChild{}
+
+	for i := range ui.errors {
+		widgets = append(widgets,
+			// toest notifications
+			layout.Rigid(func(gtx C) D {
+				return layout.Flex{
+					Axis:      layout.Horizontal,
+					Spacing:   layout.SpaceBetween,
+					Alignment: layout.Middle,
+				}.Layout(gtx,
+					layout.Rigid(func(gtx C) D {
+						return material.Label(theme, 20,
+							ui.errors[i].Error()).Layout(gtx)
+					}),
+					layout.Rigid(func(gtx C) D {
+						return material.IconButton(theme, &ui.errorClicks[i],
+							ui.icons[CLOSE_ICON], "Dimiss error").Layout(gtx)
+					}),
+				)
+			}))
+	}
+
+	return layout.Flex{
+		Axis:      layout.Vertical,
+		Spacing:   layout.SpaceStart,
+		Alignment: layout.Middle,
+	}.Layout(gtx, widgets...)
+}
+
+func drawFileEntry(gtx C, theme T, file *FileEntry) D {
+	widgets := []layout.FlexChild{
+		layout.Rigid(func(gtx C) D {
+			return material.Label(theme, 16, file.name).Layout(gtx)
+		}),
+	}
+
+	if file.progress < 0 {
+		widgets = append(widgets,
+			layout.Rigid(func(gtx C) D {
+				remove, err := widget.NewIcon(icons.NavigationClose)
+				if err != nil {
+					panic(err)
+				}
+				return material.IconButton(theme, &file.clicked, remove, "Remove file").Layout(gtx)
+			}))
+	}
+
+	entry := layout.Flex{Axis: layout.Horizontal, Spacing: layout.SpaceBetween}.Layout(gtx, widgets...)
+
+	if file.progress >= 0 {
+		return layout.Flex{Axis: layout.Vertical, Spacing: layout.SpaceStart}.Layout(gtx,
+			layout.Rigid(func(gtx C) D { return entry }),
+			layout.Rigid(func(gtx C) D {
+				return material.ProgressBar(theme, 0.5).Layout(gtx)
+			}))
+	} else {
+		return entry
+	}
 }
 
 func drawHomePage(gtx C, theme T, ui *UI) D {
@@ -251,7 +324,7 @@ func drawHomePage(gtx C, theme T, ui *UI) D {
 		layout.Flexed(0.5, func(gtx C) D {
 			return material.List(theme, ui.peersList).Layout(gtx, len(ui.peerChecks), func(gtx C, i int) D {
 				str := fmt.Sprintf("PEER-%d", i)
-				return material.CheckBox(theme, &ui.peerChecks[i], fmt.Sprintf("%v", str)).Layout(gtx)
+				return material.CheckBox(theme, &ui.peerChecks[i], str).Layout(gtx)
 			})
 		}),
 
@@ -283,12 +356,28 @@ func drawHomePage(gtx C, theme T, ui *UI) D {
 	)
 }
 
-// theme, trust peers, show notifications, download folder, copyright
+func drawProgressPage(gtx C, theme T, ui *UI) D {
+	return layout.Flex{
+		Alignment: layout.Start,
+		Axis:      layout.Vertical,
+		Spacing:   layout.SpaceEvenly,
+	}.Layout(gtx,
+		layout.Rigid(func(gtx C) D {
+			return material.H1(theme, "Sending files").Layout(gtx)
+		}),
+
+		layout.Flexed(0.5, func(gtx C) D {
+			return material.List(theme, ui.filesList).Layout(gtx, len(ui.files), func(gtx C, i int) D {
+				return drawFileEntry(gtx, theme, &ui.files[i])
+			})
+		}),
+	)
+}
 
 func drawSettingsPage(gtx C, theme T, ui *UI) D {
 	return layout.Flex{
-		Alignment: layout.Middle,
-		Spacing:   layout.SpaceEvenly,
+		Alignment: layout.Start,
+		Spacing:   layout.SpaceStart,
 		Axis:      layout.Vertical,
 	}.Layout(gtx,
 		// toggle theme
